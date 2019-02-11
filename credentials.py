c3e4fe7bfdcec01fbced00f1282493f64af20738
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-11 17:41:59 +0200

    add class

--- credentials.py
+++ credentials.py
@@ -1,15 +1,39 @@
+# class User:
+#     """
+#     Class that generates new instances of contacts.
+#     """
+
+#     user_list = [] # Empty contact list
+
+    # def __init__(self,first_name,last_name,user_name,password):
+
+    #   # docstring removed for simplicity
+
+    #     self.first_name = first_name
+    #     self.last_name = last_name
+    #     self.user_name = user_name
+    #     self.password = password
+global user_list 
 class User:
-    """
-    Class that generates new instances of contacts.
-    """
+	'''
+	Class to create user accounts and save their information
+	'''
+	# Class Variables
+	# global users_list
+	user_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		Method to define the properties for each user object will hold.
+		'''
 
-    user_list = [] # Empty contact list
+		# instance variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
 
-    def __init__(self,first_name,last_name,user_name,password):
-
-      # docstring removed for simplicity
-
-        self.first_name = first_name
-        self.last_name = last_name
-        self.user_name = user_name
-        self.password = password
+	def save_user(self):
+		'''
+		Function to save a newly created user instance
+		'''
+		User.user_list.append(self)
+		
