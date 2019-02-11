2df299e5da9527d5236465349c3965cac17dc568
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-11 15:26:27 +0200

    add test

--- user.py
+++ user.py
@@ -13,20 +13,42 @@ class User:
         self.last_name = last_name
         self.user_name = user_name
         self.password = password
-
-
-        import unittest # Importing the unittest module
-from user import User # Importing the contact class
+import unittest # Importing the unittest module
+from user import User # Importing the user class
 
 class TestUser(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the contact class behaviours.
-
+    Test class that defines test cases for the User class behaviours.
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
-    '''
+    ''' 
+     # Items up here .......
 
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("yvette","umutesiwase","umutesiwaseyvette","yvette") # create contact object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.first_name,"yvette")
+        self.assertEqual(self.new_user.last_name,"umutesiwase")
+        self.assertEqual(self.new_user.user_name,"umutesiwaseyvette")
+        self.assertEqual(self.new_user.password,"yvette")
+
+
+if __name__ == '__main__':
+    unittest.main()  
+
+
+
+      
 
 
 
