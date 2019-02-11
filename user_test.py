f6c83663b9499eea4eb20753e9a383ed0f70b146
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-11 19:05:01 +0200

    add save user test

--- user_test.py
+++ user_test.py
@@ -28,5 +28,44 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.password,"hulde")
 
 
+    def test_save_user(self):
+        '''
+        test_save_account test case to test if the account object is saved into
+         the account list
+        '''
+        self.new_user.save_user() # saving the new account
+        self.assertEqual(len(User.user_list),1)
+
+    # Items up here...
+
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Hulde","Uwizeyimana","huldeuwizeyimana@gmail.com","hulde") # new account
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+
+    #setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+    # other test cases here
+    def test_save_multiple_user(self):
+            '''
+            test_save_multiple_account to check if we can save multiple account
+            objects to our account_list
+            '''
+            self.new_user.save_user()
+            test_user = User("Hulde","Uwizeyimana","huldeuwizeyimana@gmail.com","hulde") # new account
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)    
+
+
 if __name__ == '__main__':
     unittest.main()  
