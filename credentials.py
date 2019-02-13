9a92435db152391db2161b897053ac093f5a110e
uwizeyeh <uwizeyimanahulde1@gmail.com>
2019-02-13 18:38:04 +0200

    add correct login

--- credentials.py
+++ credentials.py
@@ -53,3 +53,31 @@ class Credential:
         display_credentials method that returns the credential list
         """
         return cls.credential_list
+    @classmethod
+    def find_by_acc_name(cls,account):
+        '''
+        Method that takes in a number and returns a credential that matches that account name.
+        Args:
+            account: Account name to search for
+        Returns :
+            Credential of account that matches the account name.
+        '''
+
+        for credential in cls.credential_list:
+            if credential.account == account:
+                return credential
+
+    @classmethod
+    def credential_exist(cls,account):
+        '''
+        Method that checks if a credential exists from the credential list.
+        Args:
+            account: Account name to search if it exists
+        Returns :
+            Boolean: True or false depending if the credential exists
+        '''
+        for credential in cls.credential_list:
+            if credential.account == account:
+                    return True
+
+        return False    
--- user.py
+++ user.py
@@ -4,10 +4,9 @@ class User:
     Class that generates new instances of contacts User.
     """
     user_list = [] #Empty user list
-    def __init__(self,first_name,last_name,user_name,password):
+    def __init__(self,user_name,password):
 
-        self.first_name = first_name
-        self.last_name = last_name
+
         self.user_name = user_name
         self.password = password
 
